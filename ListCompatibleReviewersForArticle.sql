886bfcc919bc0805b1b44dd7efcccf2f365e6855
BernardoRodrigues <bernardo.qtr.21@gmail.com>
2018-11-03 14:37:16 +0000

    changed ListCompatibleReviewersForArticle script

--- ListCompatibleReviewersForArticle.sql
+++ ListCompatibleReviewersForArticle.sql
@@ -7,20 +7,21 @@ create procedure GetCompatibleReviewersForArticle
 as
 begin transaction
 	begin try
-		select 
+		select [User].name as name, [User].email as email, [User].institutionName
 		from Reviewer
 		inner join [User] on ([User].email = Reviewer.reviewerEmail)
 		left join (
-			
-			select [User].email, [User].instituionName 
-			from Author
-			inner join ArticleAuthor on (ArticleAuthor.articleId <> @articleId)
-			inner join [User] on ([User].email = Author.email)
+			select [User].email, [User].institutionName 
+			from Article
+			inner join Conference on (Conference.name = Article.conferenceName AND Conference.year = Article.conferenceYear)
+			inner join ArticleAuthor on (ArticleAuthor.articleId = Article.id)
+			inner join Author on (Author.authorEmail = ArticleAuthor.authorEmail)
+			inner join [User] on ([User].email = Author.authorEmail)
+			where ArticleAuthor.articleId <> @articleId
 		) as Aux on Aux.email = [User].email
-		where Aux.instituionName <> [User].instituionName
+		where Aux.institutionName != [User].institutionName
 	end try
 	begin catch
 		throw
 	end catch
-end transaction
 go
