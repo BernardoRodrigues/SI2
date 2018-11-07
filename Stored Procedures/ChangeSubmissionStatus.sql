fcea3b4303c7277dd0030e8502e47b2fa19a34d1
BernardoRodrigues <bernardo.qtr.21@gmail.com>
2018-11-07 02:17:01 +0000

    added procedure to change the submission status depending on the grade

---
+++ Stored Procedures/ChangeSubmissionStatus.sql
@@ -0,0 +1,82 @@
+use SI2
+
+go
+
+create procedure ChangeArticleStatus
+@conferenceName nvarchar(128),
+@conferenceYear int,
+@grade int
+as
+begin try
+	begin transaction
+		declare @articleGrade int
+				declare @articleId int
+				declare cur cursor local forward_only for
+				select Article.id, ArticleReviewer.grade
+				from Article 
+					inner join ArticleReviewer on (Article.id = ArticleReviewer.articleId)
+				where Article.conferenceName = @conferenceName AND Article.conferenceYear = @conferenceYear
+				open cur
+				fetch next from cur into @articleId, @articleGrade
+				while @@FETCH_STATUS = 0
+					begin
+						update Article
+						set accepted = 
+							case
+								when @grade <= @articleGrade
+									then 1
+								else
+									0
+								end
+							,
+							stateId = 
+								case
+									when @grade <= @articleGrade
+									then 3
+								else
+									4
+								end
+						where id = @articleId
+						fetch next from cur into @articleId, @articleGrade
+					end
+	commit transaction
+end try
+begin catch
+	declare @errorMessage nvarchar(max), 
+    @errorSeverity int, 
+    @errorState int;
+
+    select @errorMessage = ERROR_MESSAGE() + ' Line ' + cast(ERROR_LINE() as nvarchar(5)), @errorSeverity = ERROR_SEVERITY(), @errorState = ERROR_STATE();
+
+    if @@trancount > 0
+        rollback transaction;
+
+    raiserror (@errorMessage, @errorSeverity, @errorState);	
+end catch
+go
+
+create procedure ChangeSubmissionStatus
+@conferenceName nvarchar(128),
+@conferenceYear int,
+@grade int
+as
+begin try
+	begin transaction
+		if @grade is null
+			select @grade = grade from Conference where [name] = @conferenceName AND [year] = @conferenceYear
+		exec ChangeArticleStatus @conferenceName, @conferenceYear, @grade
+	commit transaction
+end try
+begin catch
+	declare @errorMessage nvarchar(max), 
+    @errorSeverity int, 
+    @errorState int;
+
+    select @errorMessage = ERROR_MESSAGE() + ' Line ' + cast(ERROR_LINE() as nvarchar(5)), @errorSeverity = ERROR_SEVERITY(), @errorState = ERROR_STATE();
+
+    if @@trancount > 0
+        rollback transaction;
+
+    raiserror (@errorMessage, @errorSeverity, @errorState);	
+end catch
+go
