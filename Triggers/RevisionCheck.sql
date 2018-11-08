7998c5d99161524d265cc0a8fb18c4af011195e0
isel42225 <42225@alunos.isel.ipl.pt>
2018-11-08 23:04:16 +0000

    Removed unecessary comments

--- Triggers/RevisionCheck.sql
+++ Triggers/RevisionCheck.sql
@@ -16,27 +16,4 @@ if @text is null or @grade is null
 	end
 declare @articleId int
 select @articleId = articleId from inserted
-
-
--- no need to check if it's compatible
--- 
-
-
--- check if reviewer is compatible
-
-
---declare @articleId int
---select @articleId = articleId from inserted
---declare @confDate date
-
---declare @confName date
---declare @confYear date
---select @confName = conferenceName, @confYear = conferenceYear from Article where id = @articleId
---select @confDate = revisionDate from Conference C where C.name = @confName and C.year = @confYear
-
---declare @revDate date
---select @revDate = revisionDate from inserted
---if(@revDate > @confDate)
---	rollback
-
 go
