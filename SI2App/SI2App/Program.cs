bb36dbf5a9725a90256c61fa3c5fe6023574dbf9
isel42225 <42225@alunos.isel.ipl.pt>
2018-12-18 11:37:56 +0000

    Added missing stored procs on EF

--- SI2App/SI2App/Program.cs
+++ SI2App/SI2App/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace SI2App
 {
@@ -14,6 +15,7 @@ namespace SI2App
         [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
